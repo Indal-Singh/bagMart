35e0c07564fca215abfec708abf40ea1963555d2
Indal-Singh <ind9546@gmail.com>
2024-10-30 23:59:48 +0530

    cart completed final update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middlewares/isLoggedIn');
 const verifyUserLoggedIn = require('../utils/verifyUserLoggedIn');
+const productModel = require('../models/product.model');
+const userModel = require('../models/user.model');
 
 router.get('/',async (req,res)=>{
     const error = req.flash('error'); // Define or fetch your error messages here
@@ -14,9 +16,41 @@ router.get('/',async (req,res)=>{
     }
 });
 
-router.get('/shop',isLoggedIn,(req,res)=>{
-    let products = [];
+router.get('/shop',isLoggedIn, async (req,res)=>{
+    let products = await productModel.find();
     return res.render('shop',{products})
+})
+
+router.get('/create-cart/:id',isLoggedIn, async (req,res)=>{
+    const id = req.params.id;
+    userId = req.user._id;
+    let upd = await userModel.findByIdAndUpdate(userId,{
+        $push:{cart:id}
+    });
+    if(upd)
+    {
+        res.send("added In cart");
+        return res.redirect("/cart");
+    }
+    else
+    {
+        res.send("Faild To add in Cart.");
+    }
+})
+
+router.get("/cart",isLoggedIn, async (req,res)=>{
+    let usreID = req.user._id;
+    let carts = await userModel.findById(usreID,'cart');
+    let products = [];
+    
+    productPromises = carts.cart.map(async (pid)=>{
+        return await productModel.findById(pid);
+    })
+
+    products = await Promise.all(productPromises);
+
+    console.log(products);
+    return res.render("cart",{products});
 })
 
 module.exports = router;
